3f40ebdc434d2e8d0795a4066ede80695d1a4e22
dralquinta <dralquinta@gmail.com>
2022-08-22 23:20:42 +0000

    Updating LICENSE

--- collector.py
+++ collector.py
@@ -1,3 +1,6 @@
+# Copyright (c) 2022 Oracle and/or its affiliates.
+# All rights reserved. The Universal Permissive License (UPL), Version 1.0 as shown at http://oss.oracle.com/licenses/upl
+
 import requests
 import sys
 import pandas as pd
--- collector.sh
+++ collector.sh
@@ -1,11 +1,6 @@
-# Copyright (c) 2021 Oracle and/or its affiliates.
-# !/bin/sh
+# Copyright (c) 2022 Oracle and/or its affiliates.
 # All rights reserved. The Universal Permissive License (UPL), Version 1.0 as shown at http://oss.oracle.com/licenses/upl
-# fireup.sh 
-#
-# Purpose: Main module which starts the application
-
-
+# !/bin/sh
 
 if [ ! -d "./venv" ] 
 then
